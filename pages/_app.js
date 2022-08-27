82d10ccb5aece3175d1460096949de2347594224
akashlil <akashlil393@gmail.com>
2022-08-27 11:34:14 +0600

    Dd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+        crossOrigin="anonymous"
+      />
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+        crossOrigin="anonymous"
+      ></script>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default Application
+export default Application;
--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,6 @@ export default function Home() {
       <Head>
         <title>Akash</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-          crossOrigin="anonymous"
-        />
       </Head>
 
       <main>
