885f65a50349216ad0626129ec87bf85bfa0b4c0
akashlil <akashlil393@gmail.com>
2022-08-27 11:22:13 +0600

    bootstapaadd

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,12 @@ export default function Home() {
       <Head>
         <title>Akash</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+          crossOrigin="anonymous"
+        />
       </Head>
 
       <main>
@@ -16,7 +22,11 @@ export default function Home() {
           wellcome<code>akash</code>
         </p>
       </main>
-
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+        crossOrigin="anonymous"
+      ></script>
       <Footer />
     </div>
   );
